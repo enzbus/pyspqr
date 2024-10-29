2c089917ad2ddfed438611dc5fa624037eba32aa
Enzo Busseti <enzo.busseti@me.com>
2024-10-30 02:16:34 +0400

    numpy input parsing

--- suitesparseqr.c
+++ suitesparseqr.c
@@ -3,7 +3,43 @@
 #include <numpy/arrayobject.h>
 #include <SuiteSparseQR_C.h>
 
+static PyObject *qr(PyObject *self, PyObject *args){
+    /* We use names of Scipy sparse CSC.*/
+    PyArrayObject *data_np;
+    PyArrayObject *indices_np;
+    PyArrayObject *indptr_np;
+    
+    PyArg_ParseTuple(args, "OOO", &data_np, &indices_np, &indptr_np);
+    
+    if (PyErr_Occurred()){
+        return NULL;
+    }
+    if (!PyArray_Check(data_np) || PyArray_TYPE(data_np) != NPY_DOUBLE || !PyArray_IS_C_CONTIGUOUS(data_np)){
+        PyErr_SetString(PyExc_TypeError,
+           "First argument must be contiguous double Numpy array.");
+        return NULL;
+    }
+    double * data_arr = PyArray_DATA(data_np);
+
+    if (!PyArray_Check(indices_np) || PyArray_TYPE(indices_np) != NPY_INT32 || !PyArray_IS_C_CONTIGUOUS(indices_np)){
+        PyErr_SetString(PyExc_TypeError,
+            "Second argument must be contiguous int32 Numpy array.");
+        return NULL;
+    }
+    int32_t * indices_arr = PyArray_DATA(indices_np);
+
+    if (!PyArray_Check(indptr_np) || PyArray_TYPE(indptr_np) != NPY_INT32 || !PyArray_IS_C_CONTIGUOUS(indptr_np)){
+        PyErr_SetString(PyExc_TypeError,
+            "Third argument must be contiguous int32 Numpy array.");
+        return NULL;
+    }
+    int32_t * indptr_arr = PyArray_DATA(indptr_np);
+
+    return PyFloat_FromDouble(0.);
+};
+
 static PyMethodDef methods[] = {
+    {"qr", qr, METH_VARARGS, "Perform sparse QR decomposition."},
     {NULL, NULL, 0, NULL}
 };
 
@@ -16,7 +52,8 @@ static struct PyModuleDef suitesparseqr = {
 };
 
 PyMODINIT_FUNC PyInit_suitesparseqr(void)
-{
+{   
+    import_array();
     return PyModuleDef_Init(&suitesparseqr);
 }
 
--- test.py
+++ test.py
@@ -1,11 +1,41 @@
 from unittest import TestCase, main
 
+import numpy as np
+
 class TestSuiteSparseQR(TestCase):
     """Unit tests for suitesparseqr."""
 
     def test_import(self):
         """Test import."""
         import suitesparseqr
+
+    def test_qr_basic(self):
+        data = np.zeros(5, dtype=float)
+        indices = np.zeros(5, dtype=np.int32)
+        indptr = np.zeros(2, dtype=np.int32)
+        from suitesparseqr import qr
+        qr(data, indices, indptr)
+        
+        with self.assertRaises(TypeError):
+            qr(data)
+
+        with self.assertRaises(TypeError):
+            qr(data, indices)
+        
+        with self.assertRaises(TypeError):
+            qr(data.astype(int), indices, indptr)    
+
+        with self.assertRaises(TypeError):
+            qr(data, indices.astype(int), indptr)   
+
+        with self.assertRaises(TypeError):
+            qr(data, indices, indptr.astype(int))   
+
+        with self.assertRaises(TypeError):
+            qr(data[::2], indices, indptr)
+
+        with self.assertRaises(TypeError):
+            qr(data, indices[::2], indptr)   
 
 if __name__ == '__main__':
     main()
