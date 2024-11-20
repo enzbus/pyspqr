fc08f47496291f35ce0f4dd35f1f8c9b7f53095f
Enzo Busseti <enzo.busseti@me.com>
2024-11-20 17:52:44 +0400

    prototyped intf in temp.py; move in library test and release

--- temp.py
+++ temp.py
@@ -2,14 +2,14 @@ from pyspqr import qr
 import scipy as sp
 import numpy as np
 
-def invert_permutation(p):
-    """Return an array s with which np.array_equal(arr[p][s], arr) is True.
-    The array_like argument p must be some permutation of 0, 1, ..., len(p)-1.
-    """
-    p = np.asanyarray(p) # in case p is a tuple, etc.
-    s = np.empty_like(p)
-    s[p] = np.arange(p.size)
-    return s
+# def invert_permutation(p):
+#     """Return an array s with which np.array_equal(arr[p][s], arr) is True.
+#     The array_like argument p must be some permutation of 0, 1, ..., len(p)-1.
+#     """
+#     #p = np.asanyarray(p) # in case p is a tuple, etc.
+#     s = np.empty_like(p)
+#     s[p] = np.arange(len(p))
+#     return s
 
 def densify(linop):
     result = np.zeros(linop.shape)
@@ -19,10 +19,32 @@ def densify(linop):
         result[:,j] = linop @ e
     return result
 
-def make_q(H, HTau):
+def make_permutation_linop(permutation):
+
+    n = len(permutation)
+    #inverse = invert_permutation(permutation)
+
+    def _matvec(vector):
+        return vector[permutation]
+
+    def _rmatvec(vector):
+        result = np.empty_like(vector)
+        result[permutation] = vector
+        return result
+
+    return sp.sparse.linalg.LinearOperator(
+        shape=(n,n),
+        matvec=_matvec,
+        rmatvec=_rmatvec,
+    )
+
+    
+
+def make_q(H, HTau, HPinv):
 
     def _q_rmatvec(input_vector):
-        result = np.array(input_vector, copy=True)
+        result = np.empty_like(input_vector)
+        result[HPinv] = input_vector
         n_reflections = H.shape[1]
         for k in range(n_reflections):
             col = H[:,k].todense().A1
@@ -35,7 +57,7 @@ def make_q(H, HTau):
         for k in range(n_reflections)[::-1]:
             col = H[:,k].todense().A1
             result -= ((col @ result) * HTau[k]) * col
-        return result
+        return result[HPinv]
 
     return sp.sparse.linalg.LinearOperator(
         shape=(m,m),
@@ -56,17 +78,21 @@ if __name__ == "__main__":
         except (ValueError, AssertionError):
             continue
     R = R.todense()
-    Q = densify(make_q(H, HTau))
+    Q = make_q(H, HTau, HPinv)
     npQ, npR = np.linalg.qr(A.todense())
     npQ[np.abs(npQ) < 1e-15] = 0.
     npR[np.abs(npR) < 1e-15] = 0.
+
+    Elinop = densify(make_permutation_linop(E))
 
     # this is super important; means that indeed the rest of the result
     # is an orthogonal matrix
     assert np.allclose(np.linalg.svd(npR)[1], np.linalg.svd(R)[1])
 
     # here how the two permutations act
-    assert np.allclose(Q[HPinv] @ R[:,invert_permutation(E)],  A.todense())
+    assert np.allclose(densify(Q) @ R @ densify(make_permutation_linop(E)),  A.todense())
+
+    assert np.allclose(densify(make_permutation_linop(E).T) @ R.T @ densify(Q.T),  A.todense().T)
     
 
 
