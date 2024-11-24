76b2e8b02f4ff7ef18e3a3b424c84039ad27e8c5
Enzo Busseti <enzo.busseti@me.com>
2024-11-24 19:28:05 +0400

    better testing

--- pyspqr/tests/test_pyspqr.py
+++ pyspqr/tests/test_pyspqr.py
@@ -19,12 +19,26 @@ import scipy as sp
 import numpy as np
 from pyspqr import qr
 
-# We use numpy.isclose for accuracy check
+############################
+# Numerical accuracy testing
+############################
+
+# This uses random matrices from scipy.sparse.random; entries are standard
+# Gaussians, sparsity patterns are varied;
+
+# We use numpy.isclose for basic accuracy check;
 ABS_ACCURACY = 1e-13
 REL_ACCURACY = 1e-13
 
-# We also compare accuracy vs dense QR Numpy/LAPACK accuracy
-ACCURACY_LOSS_DENSE = 4.
+# We also compare accuracy vs dense QR Numpy/LAPACK; it means we lose less than
+# ~1 digit accuracy;
+NORM_ACC_LOSS_VS_DENSE = 5.0
+MAX_ACC_LOSS_VS_DENSE = 10.0
+MAX_ERROR_VS_DENSE = 3 * np.finfo(float).eps # if dense has error == 0.0
+
+# Increase this to search over more random matrices; we run it locally to ~100
+# and set thresholds accordingly in order to avoid random failures in CI;
+HOW_MANY_RANDOM_NUMERICAL_TESTS = 1
 
 class TestSuiteSparseQR(TestCase):
     """Unit tests for pyspqr."""
@@ -40,11 +54,13 @@ class TestSuiteSparseQR(TestCase):
 
         self.assertLessEqual(
             np.linalg.norm(aprod - qrprod),
-            ACCURACY_LOSS_DENSE * np.linalg.norm(adenseprod - qrdenseprod))
+            NORM_ACC_LOSS_VS_DENSE * np.linalg.norm(
+                adenseprod - qrdenseprod) + MAX_ERROR_VS_DENSE)
 
         self.assertLessEqual(
             np.max(np.abs(aprod - qrprod)),
-            ACCURACY_LOSS_DENSE * np.max(np.abs(adenseprod - qrdenseprod)))
+            MAX_ACC_LOSS_VS_DENSE * np.max(np.abs(
+                adenseprod - qrdenseprod)) + MAX_ERROR_VS_DENSE)
 
         self.assertTrue(
             np.allclose(aprod, qrprod, atol=ABS_ACCURACY, rtol=REL_ACCURACY))
@@ -60,11 +76,13 @@ class TestSuiteSparseQR(TestCase):
 
         self.assertLessEqual(
             np.linalg.norm(atprod - qrtprod),
-            ACCURACY_LOSS_DENSE * np.linalg.norm(atdenseprod - qrtdenseprod))
+            NORM_ACC_LOSS_VS_DENSE * np.linalg.norm(
+                atdenseprod - qrtdenseprod) + MAX_ERROR_VS_DENSE)
 
         self.assertLessEqual(
             np.max(np.abs(atprod - qrtprod)),
-            ACCURACY_LOSS_DENSE * np.max(np.abs(atdenseprod - qrtdenseprod)))
+            MAX_ACC_LOSS_VS_DENSE * np.max(np.abs(
+                atdenseprod - qrtdenseprod)) + MAX_ERROR_VS_DENSE)
 
         self.assertTrue(
             np.allclose(atprod, qrtprod, atol=ABS_ACCURACY, rtol=REL_ACCURACY))
@@ -107,15 +125,16 @@ class TestSuiteSparseQR(TestCase):
         """Test with dense matrices."""
 
         for m, n in [(100, 100), (100, 50), (50, 100)]:
-
-            np.random.seed(0)
-            A = sp.sparse.random(m, n, density=1., format='csc')
-            self._qr_check(A)
+            for i in range(HOW_MANY_RANDOM_NUMERICAL_TESTS):
+                np.random.seed(i)
+                A = sp.sparse.random(m, n, density=1., format='csc')
+                self._qr_check(A)
 
     def test_big_sparse(self):
         """Test with random big sparse."""
 
         for m, n in [(1000, 1000), (1000, 200), (200, 1000)]:
-            np.random.seed(0)
-            A = sp.sparse.random(m, n, density = 0.01, format='csc')
-            self._qr_check(A)
+            for i in range(HOW_MANY_RANDOM_NUMERICAL_TESTS):
+                np.random.seed(i)
+                A = sp.sparse.random(m, n, density = 0.01, format='csc')
+                self._qr_check(A)
